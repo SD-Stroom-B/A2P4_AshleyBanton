a8c3384f83caab7d81d916d7e7e88f40bf28460d
AshleyBanton <a.banton@sintlucasedu.nl>
2022-06-01 21:40:25 +0200

    Assignment completed
    Assignment 2 complete

---
+++ a2p4_ashleybanton_sd/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace a2p4_ashleybanton_sd
+{
+    class Program
+    {
+
+        public enum Months
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        enum storedmonths
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            bool looping = true;
+
+            while(looping)
+            {
+                string userinput;
+
+                foreach (var v in Enum.GetValues(typeof(Months)))
+                {
+                    Console.WriteLine(v);
+                }
+
+                Console.WriteLine("Put your month of birth in:");
+                userinput = Console.ReadLine();
+
+                Months month;
+                if (Enum.TryParse<Months>($"{userinput}", out month))
+                {
+                    Console.WriteLine("Your month is: " +userinput+ "");
+                    looping = false;
+                }
+                else
+                {
+                    Console.WriteLine("Fail! Invalid month input: " + userinput + "");
+                    looping = true;
+                }
+            }
+        }
+    }
+}
